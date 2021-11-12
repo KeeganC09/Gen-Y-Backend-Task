5dd93bbf0c8dadfc7ce87c51395eb198d608be69
Keegan Colaco <keeganpaulcolaco@gmail.com>
2021-11-13 00:59:02 +0530

    updated API

---
+++ API/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const dotenv = require('dotenv');
+
+const db = require("./database/db");
+const userRoute = require('./Routes/authRoute');
+const adminRoute = require('./Routes/admin/authRoute');
+const postRoute = require('./Routes/postRoute')
+
+dotenv.config();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/', userRoute);
+app.use('/admin', adminRoute);
+app.use('/post', postRoute);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Listening at http://localhost:${process.env.PORT}`);
+});
+
